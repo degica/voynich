d2332bfe9f12e32ea5bf082636f2a6528ca40bc5
Kazunori Kajihiro <likerichie@gmail.com>
2016-02-23 20:56:53 +0900

    Automatic data key creation

--- lib/voynich/active_record/models/value.rb
+++ lib/voynich/active_record/models/value.rb
@@ -14,6 +14,7 @@ module Voynich
       validates :ciphertext, presence: true
 
       before_validation :generate_uuid, on: :create
+      before_validation :find_or_create_data_key
       before_validation :encrypt
 
       def decrypt
@@ -37,6 +38,16 @@ module Voynich
       end
 
       private
+
+      def find_or_create_data_key
+        if data_key.nil?
+          self.data_key = DataKey.find_or_create_by!(name: random_key_name, cmk_id: Voynich.kms_cmk_id)
+        end
+      end
+
+      def random_key_name
+        "auto:#{Random.rand(Voynich.auto_data_key_count)}"
+      end
 
       def generate_uuid
         self.uuid ||= SecureRandom.uuid
