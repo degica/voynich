5ddd3bed5da1f1facb5d71e9754db89f7f17f235
Kazunori Kajihiro <likerichie@gmail.com>
2016-02-24 12:44:38 +0900

    Refactor specs

--- spec/lib/active_model/model_spec.rb
+++ spec/lib/active_model/model_spec.rb
@@ -1,93 +1,98 @@
 require 'spec_helper'
 
 module Voynich::ActiveModel
-  class Target
-    include ::ActiveModel::Model
-
-    def self.belongs_to(*args); end
-
-    def self.before_save(method, options={})
-      @@before_save_callbacks[method.to_sym] = options
-    end
-    @@before_save_callbacks = {}
-
-    def save
-      @@before_save_callbacks.each do |method, options|
-        send(method)
-      end
-    end
-
-    def uuid
-      @uuid ||= SecureRandom.hex
-    end
+  class Target < ::ActiveRecord::Base
+    self.table_name_prefix = 'voynich_active_model_'
 
     include Model
 
-    attr_accessor :voynich_secret_value
-    attr_accessor :voynich_auth_secret_value
+    after_initialize do |t| 
+      t.uuid ||= SecureRandom.hex
+    end
+
     voynich_attribute :secret
-    voynich_attribute :auth_secret, context: ->(m) { m.uuid }
+    voynich_attribute :auth_secret, context: ->(m) { {uuid: m.uuid} }
   end
 
   describe Model do
+    let(:target) { Target.new }
+
     before do
       stub_kms_request
     end
 
-    describe "saving attributes" do
-      let(:target) { Target.new }
-      before do
+    describe "instance variable setter and getter" do
+      it "is nil if value doesn't exist" do
+        expect(target.secret).to be_nil
+      end
+
+      it "assigns instance variable" do
         target.secret = "super secret information"
         expect(target.secret).to eq "super secret information"
       end
+    end
 
-      it "stores secret in voynich table" do
-        target.save
-        expect(Voynich::ActiveRecord::Value.count).to eq 1
-        value = Voynich::ActiveRecord::Value.first
-        expect(value.decrypt).to eq "super secret information"
-        expect(value.data_key).to be_a Voynich::ActiveRecord::DataKey
+    describe "saving attributes" do
+      context "when secret is nil" do
+        it "doesn't create value record" do
+          target.save!
+          expect(Voynich::ActiveRecord::Value.count).to eq 0
+        end
       end
 
-      it "updates secret" do
-        target.save
-        target.secret = "yet another secret information"
-        expect(target.secret).to eq  "yet another secret information"
-        target.save
+      context "when secret value is assigned" do
+        before do
+          target.secret = "super secret information"
+          target.save!
+        end
 
-        expect(Voynich::ActiveRecord::Value.count).to eq 1
-        value = Voynich::ActiveRecord::Value.first
-        expect(value.decrypt).to eq "yet another secret information"
-      end
+        it "stores secret in voynich table" do
+          expect(Voynich::ActiveRecord::Value.count).to eq 1
+          value = Voynich::ActiveRecord::Value.first
+          expect(value.decrypt).to eq "super secret information"
+          expect(value.data_key).to be_a Voynich::ActiveRecord::DataKey
+        end
 
-      it "can decrypt the attribute" do
-        target.save
+        it "updates secret" do
+          target.secret = "yet another secret information"
+          expect(target.secret).to eq  "yet another secret information"
+          target.save!
 
-        reloaded_target = Target.new(voynich_secret_value: target.voynich_secret_value)
-        expect(reloaded_target.secret).to eq "super secret information"
+          expect(Voynich::ActiveRecord::Value.count).to eq 1
+          value = Voynich::ActiveRecord::Value.first
+          expect(value.decrypt).to eq "yet another secret information"
+        end
+
+        it "can decrypt the attribute" do
+          reloaded_target = Target.find_by!(voynich_secret_value: target.voynich_secret_value)
+          expect(reloaded_target.secret).to eq "super secret information"
+        end
       end
 
       context "with context" do
-        it "encrypts secret with context" do
+        before do
           target.auth_secret = "authenticated secret"
-          expect(target.auth_secret).to eq "authenticated secret"
-          target.save
+          target.save!
+        end
 
+        it "encrypts secret with context" do
           value = target.voynich_auth_secret_value
-          value.context = target.uuid
+          value.context = {uuid: target.uuid}
           expect(value.decrypt).to eq "authenticated secret"
         end
 
         it "updates secret with context" do
-          target.auth_secret = "authenticated secret"
-          target.save
-
           target.auth_secret = "yet another authenticated secret"
-          target.save
+          target.save!
 
           value = target.voynich_auth_secret_value
-          value.context = target.uuid
+          value.context = {uuid: target.uuid}
           expect(value.decrypt).to eq "yet another authenticated secret"
+        end
+
+        it "can decrypt the attribute" do
+          reloaded_target = Target.find_by!(voynich_secret_value: target.voynich_secret_value)
+          expect(reloaded_target.auth_secret).to eq "authenticated secret"
         end
       end
     end
