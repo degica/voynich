220f76a915a9b517439661e767ee32e90cf27763
Kazunori Kajihiro <kazunori.kajihiro@gmail.com>
2018-10-11 15:13:48 +0900

    Encoding/Decoding done in DataKey instead of KMS client

--- lib/voynich/test_support.rb
+++ lib/voynich/test_support.rb
@@ -1,13 +1,16 @@
+require 'securerandom'
+
 module Voynich
   module TestSupport
     module StubKMS
       def stub_kms_request
+        secret = SecureRandom.random_bytes(32)
         allow(Voynich).to receive(:kms_client) do
           client = Aws::KMS::Client.new(stub_responses: true)
           client.stub_responses(:generate_data_key,
-                                plaintext: 'fourty length encoded plaintext data key',
+                                plaintext: secret,
                                 ciphertext_blob: 'generated ciphertext blob')
-          client.stub_responses(:decrypt, plaintext: 'fourty length encoded plaintext data key')
+          client.stub_responses(:decrypt, plaintext: secret)
           client.stub_responses(:re_encrypt, ciphertext_blob: 'reencrypted ciphertext blob')
           client
         end
